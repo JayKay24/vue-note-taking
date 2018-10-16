de183522bbe177d7f8842118e76db3b1c931d12c
James Kinyua <jameskinyua590@gmail.com>
2018-10-16 15:58:56 +0300

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,22 @@
+const inputComponent = {
+  template: `
+    <input :placeholder="placeholder" 
+      v-model="input" class="input is-small" type="text" />
+    `,
+  props: ['placeholder'],
+  data() {
+    return { input: '' }
+  }
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    'input-component': inputComponent
+  },
+  data: {
+    notes: [],
+    timestamps: [],
+    placeholder: 'Enter a note'
+  }
+})
